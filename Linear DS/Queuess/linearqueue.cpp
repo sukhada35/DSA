2e429435739a7a1d4bbff9ca355959ce7637c6c0
sukh <149428961+sukhada35@users.noreply.github.com>
2024-08-29 23:39:53 +0530

    added for linear

--- Linear DS/Queuess/linearqueue.cpp
+++ Linear DS/Queuess/linearqueue.cpp
@@ -1,7 +1,75 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
+
+class LinearQueue 
 {
-    cout<<"Hello Queue";
+private:
+    int front, rear, capacity;
+    int *queue;
+
+public:
+    LinearQueue(int size) 
+    {
+        capacity = size;
+        queue = new int[capacity];
+        front = rear = -1;
+    }
+
+    void enqueue(int data) 
+    {
+        if (rear == capacity - 1) 
+        {
+            cout << "Queue is full, can't add more customers." << endl;
+            return;
+        } 
+        else if (front == -1) 
+        {
+            front = 0;
+        }
+        queue[++rear] = data;
+        cout << "Customer " << data << " joined the queue." << endl;
+    }
+
+    void dequeue() 
+    {
+        if (front == -1 || front > rear) 
+        {
+            cout << "Queue is empty, no customers to serve." << endl;
+            return;
+        }
+        cout << "Customer " << queue[front++] << " has been served." << endl;
+    }
+
+    void displayQueue() 
+    {
+        if (front == -1) 
+        {
+            cout << "Queue is empty." << endl;
+            return;
+        }
+        cout << "Current queue: ";
+        for (int i = front; i <= rear; i++) 
+        {
+            cout << queue[i] << " ";
+        }
+        cout << endl;
+    }
+};
+
+int main() 
+{
+    LinearQueue queue(5);
+
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.displayQueue();
+
+    queue.dequeue();
+    queue.displayQueue();
+
+    queue.enqueue(4);
+    queue.displayQueue();
+
     return 0;
 }
